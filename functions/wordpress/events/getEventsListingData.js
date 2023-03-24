5a52201ac4c10691cdc076fcb51060f21df80594
Bob_Smith <reversetype@gmail.com>
2023-03-24 14:18:56 -0400

    📝 docs(formatBlockData.js): update comment to reflect new functionality of getEventsListingData function 🚀 perf(formatBlockData.js): add support for event category filtering in getEventsListingData function
    🔀 merge(getEventsListingData.js): merge changes to support retrieving events listings from a category
    ✨ feat(getEventsListingData.js): add support for retrieving events listings from a category if no IDs are provided
    
    🔥 refactor(connector.js): remove console.log statements
    ✨ feat(queryEventsListingAttributes.js): add queryEventsCategoryAttributes query to get events by category

--- functions/wordpress/events/getEventsListingData.js
+++ functions/wordpress/events/getEventsListingData.js
@@ -1,43 +1,71 @@
 import { initializeWpApollo } from '@/lib/wordpress/connector';
-import queryEventsListingAttributes from '@/lib/wordpress/events-listing/queryEventsListingAttributes';
+import queryEventsListingAttributes, { queryEventsCategoryAttributes } from '@/lib/wordpress/events-listing/queryEventsListingAttributes';
 
 /**
 
 Retrieve news listings by their IDs.
 @param {Array} ids - Array of news listing IDs to retrieve.
+@param {Number} category - Category ID to retrieve events listings from if no IDs are provided.
 @return {Promise<Array>} - Promise that resolves to an array of news listings, or an error object.
 */
-export default async function getEventsListingData(ids) {
+export default async function getEventsListingData(ids, category) {
     // No IDs? Bail...
-    if (!ids || !Array.isArray(ids)) {
-        return { isError: true, message: 'Invalid IDs provided.' };
+    if ((!ids || !Array.isArray(ids)) && !category) {
+        return { isError: true, message: 'Invalid IDs or category provided.' };
     }
     // Get/create Apollo instance.
     const apolloClient = initializeWpApollo();
 
-    // Execute query for each ID.
-    const eventsListings = await Promise.all(
-        ids.map(async (id) => {
-            const eventsListing = await apolloClient
-                .query({
-                    query: queryEventsListingAttributes,
-                    variables: {
-                        id: id,
-                    },
-                })
-                .then((result) => {
-                    return result?.data?.event ?? null
-                })
-                .catch((error) => {
-                    return {
-                        isError: true,
-                        message: error.message,
-                    };
-                });
+    let eventsListings = [];
 
-            return eventsListing;
-        })
-    );
+    // If IDs are provided, execute query for each ID.
+    if (ids && Array.isArray(ids)) {
+        eventsListings = await Promise.all(
+            ids.map(async (id) => {
+                const eventsListing = await apolloClient
+                    .query({
+                        query: queryEventsListingAttributes,
+                        variables: {
+                            id: id,
+                        },
+                    })
+                    .then((result) => {
+                        return result?.data?.event ?? null;
+                    })
+                    .catch((error) => {
+                        return {
+                            isError: true,
+                            message: error.message,
+                        };
+                    });
+
+                return eventsListing;
+            })
+        );
+    }
+    // If no IDs are provided, retrieve events listings from the specified category.
+    if (category) {
+
+        const result = await apolloClient
+            .query({
+                query: queryEventsCategoryAttributes,
+                variables: {
+                    category: category,
+                },
+            })
+            .then((result) => {
+                const eventsInCategory = result?.data?.eventCategory?.events?.nodes || [];
+                return eventsInCategory
+            })
+            .catch((error) => {
+                return {
+                    isError: true,
+                    message: error.message,
+                };
+            });
+
+        return result;
+    }
 
     return eventsListings;
 }
