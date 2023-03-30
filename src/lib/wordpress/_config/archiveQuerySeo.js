e1a60f6099b888bfd92a00b97da3e5aeae490bb8
Bob_Smith <reversetype@gmail.com>
2023-03-29 21:57:56 -0400

    🐛 fix(BlockDepartmentSelect.js): remove unused imports and variables ✨ feat(BlockDepartmentSelect.js): add selectedDepartment prop to DepartmentSelector and handleDepartmentChange prop to handle dropdown change
    🔨 refactor(BlockDepartmentSelect.js): remove unused innerBlocks and props variables
    🐛 fix(BlockDepartmentSelect.js): fix programDepartmentsHold variable to programDepartments
    ✨ feat(BlockDepartmentSelect.js): add handleDepartmentChange function to handle department selection
    
    🚀 feat(displayBlock.js): add support for eab/program-directory block with BlockDepartmentSelect component
    
    🔀 merge: merge branch 'feature/program-directory' into develop
    🔀 merge: merge branch 'feature/departments-archive' into develop
    
    ✨ feat(queryDepartmentsArchive.js): add query to retrieve departments archive data

--- src/lib/wordpress/_config/archiveQuerySeo.js
+++ src/lib/wordpress/_config/archiveQuerySeo.js
@@ -1,10 +1,16 @@
 import queryPostsArchive from '@/lib/wordpress/posts/queryPostsArchive'
+import queryDepartmentsArchive from '@/lib/wordpress/departments/queryDepartmentsArchive'
 
 // Define SEO for archives.
 const archiveQuerySeo = {
   post: {
     query: queryPostsArchive,
     title: 'Blog',
+    description: ''
+  },
+  department: {
+    query: queryDepartmentsArchive,
+    title: 'Departments',
     description: ''
   }
 }
