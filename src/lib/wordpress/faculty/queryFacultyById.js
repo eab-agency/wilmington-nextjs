57d8abcc496f18681a716b6bef2c1c0ee79607be
Bob_Smith <reversetype@gmail.com>
2023-04-01 19:42:50 -0400

    🐛 fix(next.config.js): add missing comma to formats array 🎨 style(Image.js): change layout prop to fill prop 🎨 style(Image.module.css): add object-fit and position properties to hasImageFill class ✨ feat(getPostTypeById.js): add queryFacultyById function to postTypeQuery object
    ✨ feat(postTypes.js): add facultyMember post type
    ✨ feat(facultyAcfFields.js): add query partial to retrieve faculty acf fields
    
    🔨 refactor(queryFacultyById.js): extract singleFacultyFragment to a named export
    ✨ feat(queryFacultyById.js): add departments to singleFacultyFragment
    🔨 refactor(queryFacultyById.js): add defaultPageData to queryFacultyById
    🔨 refactor(queryFacultyById.js): change image size default value from LARGE to MEDIUM
    
    ✨ feat(faculty/[...slug].js): add Faculty component and its dependencies

--- src/lib/wordpress/faculty/queryFacultyById.js
+++ src/lib/wordpress/faculty/queryFacultyById.js
@@ -1,35 +1,41 @@
-import featuredImagePostFields from '@/lib/wordpress/_query-partials/featuredImagePostFields'
 import { gql } from '@apollo/client'
+import defaultPageData from '@/lib/wordpress/_query-partials/defaultPageData'
+import globalPostFields from '@/lib/wordpress/_query-partials/globalPostFields'
+import featuredImagePostFields from '@/lib/wordpress/_query-partials/featuredImagePostFields'
+import facultyAcfFields from '@/lib/wordpress/_query-partials/facultyAcfFields'
+import seoPostFields from '@/lib/wordpress/_query-partials/seoPostFields'
 
-// Fragment: retrieve single post fields.
-const singleFacultyFragment = gql`
+
+// Fragment: retrieve single page fields.
+export const singleFacultyFragment = gql`
   fragment SingleFacultyFields on FacultyMember {
-       databaseId
-       link
-    facultyFields {
-      faculty {
-        first
-        last
-        phone
-        email
-        position
+     ${globalPostFields}
+    blocksJSON
+    excerpt
+     departments {
+      nodes {
+        name
       }
     }
+    ${facultyAcfFields}
+    ${seoPostFields}
     ${featuredImagePostFields}
   }
 `
+
 // Query: retrieve post by specified identifier.
 const queryFacultyById = gql`
   query GET_FACULTY_BY_ID(
-    $id: ID!
-    $imageSize: MediaItemSizeEnum = LARGE
-
+     $id: ID!
+    $idType: FacultyMemberIdType = URI
+    $imageSize: MediaItemSizeEnum = MEDIUM
   ) {
-    facultyMember(id: $id, idType: DATABASE_ID) {
-        ...SingleFacultyFields
-    }
-  },
-    ${singleFacultyFragment}
+  ${defaultPageData}
+  facultyMember(id: $id, idType: $idType) {
+    ...SingleFacultyFields
+  }
+}
+${singleFacultyFragment}
 `
 
 export default queryFacultyById
---
+++ src/pages/faculty/[...slug].js
@@ -0,0 +1,96 @@
+import FeaturedImage from '@/components/common/FeaturedImage'
+import Container from '@/components/atoms/Container'
+import RichText from '@/components/atoms/RichText'
+import Layout from '@/components/common/Layout'
+import Blocks from '@/components/molecules/Blocks'
+import getPagePropTypes from '@/functions/getPagePropTypes'
+import getPostTypeStaticPaths from '@/functions/wordpress/postTypes/getPostTypeStaticPaths'
+import getPostTypeStaticProps from '@/functions/wordpress/postTypes/getPostTypeStaticProps'
+import Image from '@/components/atoms/Image';
+
+// Define route post type.
+const postType = 'facultyMember'
+
+/**
+ * Render the Faculty component.
+ *
+ * @param  {object}  props             The component attributes as props.
+ * @param  {boolean} props.archive     Whether displaying single post (false) or archive (true).
+ * @param  {boolean} props.dateArchive Whether displaying single post (false) or date-based archive (true).
+ * @param  {string}  props.day         Date query: day.
+ * @param  {string}  props.month       Date query: month.
+ * @param  {object}  props.pagination  Archive pagination data from WordPress.
+ * @param  {object}  props.post        Post data from WordPress.
+ * @param  {Array}   props.posts       Array of post data from WordPress.
+ * @param  {string}  props.year        Date query: year.
+ * @return {Element}                   The Page component.
+ */
+export default function Faculty({
+    post
+}) {
+    const { email, facebook, first, last, instagram, linkedin, location, phone, position, tiktok, twitter, youtube } = post?.facultyFields?.faculty
+
+    const { name: departmentName } = post?.departments?.nodes[0]
+
+    const { altText, sourceUrl } = post?.featuredImage?.node
+
+    let positionTitle = position;
+
+    // checking if departmentName is not empty so we can add a comma
+    if (departmentName !== "") {
+        positionTitle += `, ${departmentName}`;
+    }
+
+    return (
+        <Layout seo={{ ...post?.seo }}>
+            <Container>
+                <article className="innerWrap">
+                    <Image id="featured-img" url={sourceUrl} alt={altText} width='300' height='200' />
+                    <RichText tag="h1">{post?.title}</RichText>
+                    <RichText tag="h2">{positionTitle}</RichText>
+                    <Blocks blocks={post?.blocks} />
+                </article>
+                <div className="sidebar">
+                    First: {first}<br />
+                    Last: {last}<br />
+                    Email: {email}<br />
+                    Facebook: {facebook}<br />
+                    Instagram: {instagram}<br />
+                    Linkedin: {linkedin}<br />
+                    Location: {location}<br />
+                    Phone: {phone}<br />
+                    Position: {position}<br />
+                    Tiktok: {tiktok}<br />
+                    Twitter: {twitter}<br />
+                    Youtube: {youtube}<br />
+                </div>
+            </Container>
+        </Layout>
+    )
+}
+
+/**
+ * Get post static paths.
+ *
+ * @return {object} Object consisting of array of paths and fallback setting.
+ */
+export async function getStaticPaths() {
+    return await getPostTypeStaticPaths(postType)
+}
+
+/**
+ * Get post static props.
+ *
+ * @param  {object}  context             Context for current post.
+ * @param  {object}  context.params      Route parameters for current post.
+ * @param  {boolean} context.preview     Whether requesting preview of post.
+ * @param  {object}  context.previewData Post preview data.
+ * @return {object}                      Post props.
+ */
+export async function getStaticProps({ params, preview, previewData }) {
+    return getPostTypeStaticProps(params, postType, preview, previewData)
+}
+
+Faculty.propTypes = {
+    ...getPagePropTypes(postType),
+}
