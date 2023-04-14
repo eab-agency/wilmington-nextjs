447ac187316baf3e545a3e4e2fd28ff09411fb3d
Bob_Smith <reversetype@gmail.com>
2023-04-14 18:04:09 -0400

    🐛 fix(fetchRedirects.js): fix SITE_URL concatenation The SITE_URL environment variable was not being concatenated correctly with the API endpoint, causing the fetch to fail. This commit fixes the concatenation by adding a missing forward slash.

--- src/lib/wordpress/fetchRedirects.js
+++ src/lib/wordpress/fetchRedirects.js
@@ -1,6 +1,6 @@
 const fetchRedirects = () => {
   const url = process.env.SITE_URL
-    ? `${process.env.SITE_URL}/api/wordpress/redirects`
+    ? `${process.env.SITE_URL}api/wordpress/redirects`
     : 'https://wilmington-nexjs.vercel.app/api/wordpress/redirects'
 
   return fetch(url)
