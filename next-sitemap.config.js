09940d7885841596999bc29471df72eadc6cd238
Bob_Smith <reversetype@gmail.com>
2023-04-14 15:57:30 -0400

    🔧 chore(.gitignore): add sitemap files to ignore list ✨ feat(next-sitemap.config.js): add next-sitemap configuration file 🔧 chore(package.json): add next-sitemap to postbuild script and rimraf to clean script, rename dev script to develop

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+// @see https://github.com/iamvishnusankar/next-sitemap
+module.exports = {
+  siteUrl: process.env.SITE_URL || 'https://wilmington-nexjs.vercel.app/',
+  generateRobotsTxt: true,
+  changefreq: 'weekly'
+}
