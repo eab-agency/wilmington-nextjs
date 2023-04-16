d0912519b95ebaf2cc6d0f73d3e0418c6c26f837
Andrei Olea <aolea@eab.com>
2023-04-16 00:25:29 -0400

    🎨 style(NewsListing.js): add responsive carousel to news listing 🎨 style(NewsListing.module.scss): comment out overflow-x property 🎨 style(_reactCarousel.scss): add styles for carousel dots

--- src/components/organisms/NewsListing/NewsListing.js
+++ src/components/organisms/NewsListing/NewsListing.js
@@ -1,7 +1,27 @@
 /* eslint-disable no-unused-vars */
 import NewsPost from '@/components/archive/NewsPost'
 import Button from '@/components/atoms/Buttons/Button'
+import Carousel from 'react-multi-carousel'
 import * as styles from './NewsListing.module.scss'
+
+const responsive = {
+  superLargeDesktop: {
+    breakpoint: { max: 4000, min: 3000 },
+    items: 4
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
 
 function NewsListing({ listing_title, posts, showImage, listing_display }) {
   // if posts.isError is true then return posts.message
@@ -17,13 +37,18 @@ function NewsListing({ listing_title, posts, showImage, listing_display }) {
         <h2>{listing_title}</h2>
       </div>
       <div className={styles.newsContainer}>
-        <ul>
+        <Carousel responsive={responsive} showDots={true}>
           {posts.map((item, index) => (
-            <li key={index}>
-              <NewsPost post={item} ctx={undefined} showImage={showImage} />
-            </li>
+            // <li key={index}>
+            <NewsPost
+              key={index}
+              post={item}
+              ctx={undefined}
+              showImage={showImage}
+            />
+            // </li>
           ))}
-        </ul>
+        </Carousel>
         <Button url="/news" text="View All News" />
       </div>
     </section>
