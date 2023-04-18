71b32e1f4039be4bfb32917366bb37b877ce25d4
Bob_Smith <reversetype@gmail.com>
2023-04-18 00:41:05 -0400

    🐛 fix(formatBlockData.js): add news_category to getNewsListingData function call 🚀 perf(getNewsListingData.js): add support for retrieving news by category 🚀 perf(queryEventsListingAttributes.js): increase number of events retrieved The formatBlockData.js file now passes the news_category attribute to the getNewsListingData function. This allows the function to retrieve news listings by category. The getNewsListingData.js file now has support for retrieving news by category. If no IDs or category are provided, all news is retrieved. The queryEventsListingAttributes.js file now retrieves four events instead of three.

--- src/functions/wordpress/news/getNewsListingData.js
+++ src/functions/wordpress/news/getNewsListingData.js
@@ -1,6 +1,7 @@
 import { initializeWpApollo } from '@/lib/wordpress/connector'
 import queryNewsListingAttributes, {
-  queryAllNewsListings
+  queryAllNewsListings,
+  queryNewsCategoryAttributes
 } from '@/lib/wordpress/news-listing/queryNewsListingAttributes'
 
 /**
@@ -9,18 +10,18 @@ Retrieve news listings by their IDs.
 @param {Array} ids - Array of news listing IDs to retrieve.
 @return {Promise<Array>} - Promise that resolves to an array of news listings, or an error object.
 */
-export default async function getNewsListingData(ids) {
-  // Get/create Apollo instance.
-  const apolloClient = initializeWpApollo()
+export default async function getNewsListingData(ids, category) {
+  // No IDs and category? Get all news.
+  if (!ids && !category) {
+    const apolloClient = initializeWpApollo()
 
-  if (!ids || !Array.isArray(ids)) {
-    const allNewsListings = await apolloClient
+    const allNews = await apolloClient
       .query({
-        query: queryAllNewsListings,
-        variables: {}
+        query: queryAllNewsListings
       })
       .then((result) => {
-        return result?.data?.news?.nodes ?? null
+        const events = result?.data?.news?.nodes || []
+        return events
       })
       .catch((error) => {
         return {
@@ -29,33 +30,58 @@ export default async function getNewsListingData(ids) {
         }
       })
 
-    return allNewsListings
-
-    // return { isError: true, message: 'Invalid IDs provided.' }
-  } else {
-    // Execute query for each ID.
-    const newsListings = await Promise.all(
-      ids.map(async (id) => {
-        const newsListing = await apolloClient
-          .query({
-            query: queryNewsListingAttributes,
-            variables: {
-              id: id
-            }
-          })
-          .then((result) => {
-            return result?.data?.article ?? null
-          })
-          .catch((error) => {
-            return {
-              isError: true,
-              message: error.message
-            }
-          })
-
-        return newsListing
-      })
-    )
-    return newsListings
+    return allNews
   }
+
+  // No IDs? Get news from category.
+  if (!ids || !Array.isArray(ids)) {
+    const apolloClient = initializeWpApollo()
+
+    const result = await apolloClient
+      .query({
+        query: queryNewsCategoryAttributes,
+        variables: {
+          category: category
+        }
+      })
+      .then((result) => {
+        return result?.data?.newsCategory?.news?.nodes ?? []
+      })
+      .catch((error) => {
+        return {
+          isError: true,
+          message: error.message
+        }
+      })
+
+    return result
+  }
+
+  // Get/create Apollo instance and execute query for each ID.
+  const apolloClient = initializeWpApollo()
+
+  // Execute query for each ID.
+  const newsListings = await Promise.all(
+    ids.map(async (id) => {
+      const newsListing = await apolloClient
+        .query({
+          query: queryNewsListingAttributes,
+          variables: {
+            id: id
+          }
+        })
+        .then((result) => {
+          return result?.data?.article ?? null
+        })
+        .catch((error) => {
+          return {
+            isError: true,
+            message: error.message
+          }
+        })
+
+      return newsListing
+    })
+  )
+  return newsListings
 }
