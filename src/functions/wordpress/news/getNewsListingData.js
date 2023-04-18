c1a3a82c7a266e94ddbd07799b512fad74a228ba
Bob_Smith <reversetype@gmail.com>
2023-04-18 00:02:03 -0400

    🔀 merge(getNewsListingData.js): merge queryAllNewsListings into getNewsListingData ✨ feat(getNewsListingData.js): add support for retrieving all news listings 🐛 fix(getNewsListingData.js): fix error message when no IDs are provided The queryAllNewsListings function is now merged into getNewsListingData, which simplifies the codebase and reduces the number of queries made to the server. Support for retrieving all news listings is now available by passing no IDs to the function. The error message when no IDs are provided has been fixed to return the correct error message.

--- src/functions/wordpress/news/getNewsListingData.js
+++ src/functions/wordpress/news/getNewsListingData.js
@@ -1,5 +1,7 @@
 import { initializeWpApollo } from '@/lib/wordpress/connector'
-import queryNewsListingAttributes from '@/lib/wordpress/news-listing/queryNewsListingAttributes'
+import queryNewsListingAttributes, {
+  queryAllNewsListings
+} from '@/lib/wordpress/news-listing/queryNewsListingAttributes'
 
 /**
 
@@ -8,36 +10,52 @@ Retrieve news listings by their IDs.
 @return {Promise<Array>} - Promise that resolves to an array of news listings, or an error object.
 */
 export default async function getNewsListingData(ids) {
-  // No IDs? Bail...
-  if (!ids || !Array.isArray(ids)) {
-    return { isError: true, message: 'Invalid IDs provided.' }
-  }
   // Get/create Apollo instance.
   const apolloClient = initializeWpApollo()
 
-  // Execute query for each ID.
-  const newsListings = await Promise.all(
-    ids.map(async (id) => {
-      const newsListing = await apolloClient
-        .query({
-          query: queryNewsListingAttributes,
-          variables: {
-            id: id
-          }
-        })
-        .then((result) => {
-          return result?.data?.article ?? null
-        })
-        .catch((error) => {
-          return {
-            isError: true,
-            message: error.message
-          }
-        })
+  if (!ids || !Array.isArray(ids)) {
+    const allNewsListings = await apolloClient
+      .query({
+        query: queryAllNewsListings,
+        variables: {}
+      })
+      .then((result) => {
+        return result?.data?.news?.nodes ?? null
+      })
+      .catch((error) => {
+        return {
+          isError: true,
+          message: error.message
+        }
+      })
 
-      return newsListing
-    })
-  )
+    return allNewsListings
 
-  return newsListings
+    // return { isError: true, message: 'Invalid IDs provided.' }
+  } else {
+    // Execute query for each ID.
+    const newsListings = await Promise.all(
+      ids.map(async (id) => {
+        const newsListing = await apolloClient
+          .query({
+            query: queryNewsListingAttributes,
+            variables: {
+              id: id
+            }
+          })
+          .then((result) => {
+            return result?.data?.article ?? null
+          })
+          .catch((error) => {
+            return {
+              isError: true,
+              message: error.message
+            }
+          })
+
+        return newsListing
+      })
+    )
+    return newsListings
+  }
 }
