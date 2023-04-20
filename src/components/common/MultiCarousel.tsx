abe740f10e74abd92017cfda3162fe37a95afe0f
Andrei Olea <aolea@eab.com>
2023-04-20 17:37:06 -0400

    🎨 style(MultiCarousel.tsx): add containerClass prop and import CarouselProps from react-multi-carousel 🎨 style(NewsListing.js, NewsListing.module.scss): refactor NewsListing carousel styles and add frontPage class 🎨 style(AlgoliaSearch.module.scss): remove commented out code and add align-items to searchContainer class
    🎨 style(_reactCarousel.scss): remove commented out styles and add new styles for RTL support and accessibility

--- src/components/common/MultiCarousel.tsx
+++ src/components/common/MultiCarousel.tsx
@@ -1,35 +1,22 @@
 'use client'
 
 import { ReactNode } from 'react'
-import Carousel from 'react-multi-carousel'
+import Carousel, { CarouselProps } from 'react-multi-carousel'
+// import 'react-multi-carousel/lib/styles.css'
 
-interface MultiCarouselProps {
+interface MultiCarouselProps extends CarouselProps {
   children: ReactNode
+  containerClass?: string
 }
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 4
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-}
-
-const MultiCarousel = ({ children }: MultiCarouselProps) => {
+const MultiCarousel = ({
+  children,
+  containerClass = '',
+  ...carouselProps
+}: MultiCarouselProps) => {
   return (
     <>
-      <Carousel responsive={responsive} showDots={true}>
+      <Carousel containerClass={containerClass} {...carouselProps}>
         {children}
       </Carousel>
     </>
