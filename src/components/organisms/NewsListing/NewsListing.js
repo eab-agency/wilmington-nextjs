4ffdaea34fcf99043dba53d69cf76ddb923d4dad
Andrei Olea <aolea@eab.com>
2023-04-20 17:37:06 -0400

    🎨 style(MultiCarousel.tsx): add containerClass prop and import CarouselProps from react-multi-carousel 🎨 style(NewsListing.js, NewsListing.module.scss): refactor NewsListing carousel styles and add frontPage class 🎨 style(AlgoliaSearch.module.scss): remove commented out code and add align-items to searchContainer class
    🎨 style(_reactCarousel.scss): remove commented out styles and add new styles for RTL support and accessibility

--- src/components/organisms/NewsListing/NewsListing.js
+++ src/components/organisms/NewsListing/NewsListing.js
@@ -2,51 +2,66 @@
 import NewsPost from '@/components/archive/NewsPost'
 import Button from '@/components/atoms/Buttons/Button'
 import MultiCarousel from '@/components/common/MultiCarousel'
+// import { useRouter } from 'next/router'
 import styles from './NewsListing.module.scss'
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 4000, min: 3000 },
-    items: 4
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1
-  }
-}
-
 function NewsListing({ listing_title, posts, showImage, listing_display }) {
+  // const router = useRouter()
+  // const isFrontPage = router.asPath === '/'
+
   // if posts.isError is true then return posts.message
   if (posts.isError) {
     return <div>{posts.message}</div>
   }
 
+  const responsive = {
+    // [isFrontPage ? 'superLargeDesktop' : 'desktop']: {},
+    // superLargeDesktop: {
+    //   breakpoint: { max: 4000, min: 1200 },
+    //   items: [isFrontPage ? 5 : 4]
+    // },
+    superLargeDesktop: {
+      breakpoint: { max: 4000, min: 1200 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 1200, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      partialVisibilityGutter: 40
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      partialVisibilityGutter: 40
+    }
+  }
+
   return (
-    <section className={`${styles.newsSection}  ${styles.homePage}`}>
-      {/* <pre>FILE: NewsListing.tsx</pre> */}
+    <section className={styles.newsSection}>
       <div className={styles.sectionHead}>
         <div className={styles.sectionTag}>News</div>
         <h2>{listing_title}</h2>
       </div>
       <div className={styles.newsContainer}>
-        <MultiCarousel>
+        <MultiCarousel
+          responsive={responsive}
+          showDots={true}
+          containerClass={styles.newsCarousel}
+          // containerClass={`${styles.newsCarousel} ${
+          //   isFrontPage ? styles.frontPage : ''
+          // }`}
+        >
           {posts.map((item, index) => (
-            // <li key={index}>
             <NewsPost
               key={index}
               post={item}
               ctx={undefined}
               showImage={showImage}
             />
-            // </li>
           ))}
         </MultiCarousel>
         <Button url="/news" text="View All News" />
