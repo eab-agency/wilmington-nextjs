d85e09bc7fbbb3e3131da1bf819eb7be18b180ec
Andrei Olea <aolea@eab.com>
2023-04-21 18:27:31 -0400

    🎨 style(BlockFeaturedPrograms.js): adjust responsive breakpoints for carousel 🎨 style(BlockFeaturedPrograms.js): add infinite scrolling to carousel 🎨 style(BlockFeaturedPrograms.js): adjust carousel container styles 🎨 style(ProgramCard.js): wrap ProgramCard in Link component 🎨 style(ProgramCard.js): adjust image styles 🎨 style(ProgramCard.module.scss): adjust image styles 🎨 style(EventsListing.module.scss): remove margin-top 🎨 style(NewsListing.module.scss): remove margin-bottom 🎨 style(_typography.scss): adjust h3 font-size 🎨 style(_global.scss): add margin to section 🎨 style(_borders.scss): add empty mixin for future use

--- src/components/molecules/ProgramCard/ProgramCard.js
+++ src/components/molecules/ProgramCard/ProgramCard.js
@@ -1,22 +1,25 @@
-import * as React from 'react'
-import Link from '@/components/common/Link'
 import Image from '@/components/atoms/Image'
+import Link from '@/components/common/Link'
 import ParsedContent from '@/functions/parsedContent'
 import styles from './ProgramCard.module.scss'
 
 const ProgramCard = ({ title, excerpt, link, image }) => {
   return (
     <div className={styles.programCard}>
-      {image && (
-        <Image
-          url={image.mediaItemUrl}
-          alt={image.altText}
-          imageMeta={{ mediaDetails: image.mediaDetails }}
-        />
-      )}
-      <h3>{title}</h3>
-      <ParsedContent content={excerpt} />
-      {link && <Link href={link}>View Program</Link>}
+      <Link href={link}>
+        {image && (
+          <Image
+            url={image.sourceUrl}
+            alt={image.altText}
+            imageMeta={{ mediaDetails: image.mediaDetails }}
+            className={styles.image}
+          />
+        )}
+        <h3>{title}</h3>
+        <ParsedContent content={excerpt} />
+        {/* {link && <Link href={link}>View Program</Link>}
+         */}
+      </Link>
     </div>
   )
 }
