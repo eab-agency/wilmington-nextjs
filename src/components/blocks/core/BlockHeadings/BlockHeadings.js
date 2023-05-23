ded761b795fff2f5f38b0bc895850d71b84695d4
Bob_Smith <reversetype@gmail.com>
2023-05-23 17:30:01 -0400

    🐛 fix(Image.js): add default alt text when none is provided 🔥 chore(BlockHeadings.js): remove unused PropTypes import and prop types 🔥 chore(HomeHero.js): remove unused React import The Image component now provides default alt text when none is provided, which improves accessibility. The PropTypes import and prop types in BlockHeadings.js were removed as they were unused. The React import in HomeHero.js was removed as it was also unused.

--- src/components/blocks/core/BlockHeadings/BlockHeadings.js
+++ src/components/blocks/core/BlockHeadings/BlockHeadings.js
@@ -1,8 +1,6 @@
 import Heading from '@/components/atoms/Heading'
 import getBlockStyles from '@/functions/wordpress/blocks/getBlockStyles'
 import cn from 'classnames'
-import PropTypes from 'prop-types'
-import React from 'react'
 
 export default function BlockHeadings({
   anchor,
@@ -35,15 +33,4 @@ export default function BlockHeadings({
       {content}
     </Heading>
   )
-}
-
-BlockHeadings.propTypes = {
-  anchor: PropTypes.string,
-  className: PropTypes.string,
-  content: PropTypes.string.isRequired,
-  fontSize: PropTypes.string,
-  textColor: PropTypes.string,
-  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]).isRequired,
-  style: PropTypes.string,
-  textAlign: PropTypes.oneOf(['left', 'center', 'right'])
 }
