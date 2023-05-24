f50c8fa12c8932641e001c7f1fc09d1285bc2d8c
Bob_Smith <reversetype@gmail.com>
2023-05-24 16:01:43 -0400

    add not found to programs

--- src/app/programs/[slug]/[course]/page.js
+++ src/app/programs/[slug]/[course]/page.js
@@ -3,6 +3,7 @@ import RichText from '@/components/atoms/RichText'
 import Blocks from '@/components/molecules/Blocks'
 import getPostTypeStaticPaths from '@/functions/wordpress/postTypes/getPostTypeStaticPaths'
 import getPostTypeStaticProps from '@/functions/wordpress/postTypes/getPostTypeStaticProps'
+import { notFound } from 'next/navigation'
 
 const postType = 'program'
 
@@ -27,12 +28,16 @@ export async function generateStaticParams() {
 const programChildPage = async ({ params }) => {
   const id = `/programs/${params?.slug}/${params?.course}`
   const { props } = await getPostTypeStaticProps({ slug: id }, postType)
-  const { post } = props
+  const { post } = props || {}
 
   // Filter the blocks array for core/heading blocks with level attribute equal to 2
   const jumpLinks = post?.blocks?.filter(
     (block) => block.name === 'core/heading' && block.attributes.level === 2
   )
+
+  if (!post) {
+    notFound()
+  }
 
   return (
     <div>
--- src/app/programs/[slug]/page.js
+++ src/app/programs/[slug]/page.js
@@ -3,6 +3,7 @@ import RichText from '@/components/atoms/RichText'
 import Blocks from '@/components/molecules/Blocks'
 import getPostTypeStaticPaths from '@/functions/wordpress/postTypes/getPostTypeStaticPaths'
 import getPostTypeStaticProps from '@/functions/wordpress/postTypes/getPostTypeStaticProps'
+import { notFound } from 'next/navigation'
 
 const postType = 'program'
 
@@ -27,40 +28,46 @@ export async function generateStaticParams() {
 export default async function Page({ params }) {
   const id = `/programs/${params?.slug}`
   const { props } = await getPostTypeStaticProps({ slug: id }, postType)
-  const { post } = props
+  const { post } = props || {}
 
   // Filter the blocks array for core/heading blocks with level attribute equal to 2
   const jumpLinks = post?.blocks?.filter(
     (block) => block.name === 'core/heading' && block.attributes.level === 2
   )
 
+  if (!post) {
+    notFound()
+  }
+
   return (
     <Container>
-      <article className="innerWrap">
-        <RichText tag="h1">{post?.title}</RichText>
+   
+        <article className="innerWrap">
+          <RichText tag="h1">{post?.title}</RichText>
 
-        {/* Render jump links */}
-        {jumpLinks.length > 0 && (
-          <>
-            <h2>On this page</h2>
-            <ul>
-              {jumpLinks.map((block, index) => (
-                <li key={index}>
-                  <a href={`#${block.attributes.anchor}`}>
-                    {block.attributes.content}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
+          {/* Render jump links */}
+          {jumpLinks.length > 0 && (
+            <>
+              <h2>On this page</h2>
+              <ul>
+                {jumpLinks.map((block, index) => (
+                  <li key={index}>
+                    <a href={`#${block.attributes.anchor}`}>
+                      {block.attributes.content}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
 
-        <Blocks
-          blocks={post?.blocks}
-          departments={post?.departments?.nodes}
-          programOrgRelationship={post?.programOrgRelationship?.programorg}
-        />
-      </article>
+          <Blocks
+            blocks={post?.blocks}
+            departments={post?.departments?.nodes}
+            programOrgRelationship={post?.programOrgRelationship?.programorg}
+          />
+        </article>
+
     </Container>
   )
 }
