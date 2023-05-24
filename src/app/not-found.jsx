92bb37617439a7c5c70aa8e7c4404e0f006c065d
Bob_Smith <reversetype@gmail.com>
2023-05-24 16:27:54 -0400

    🎨 style(not-found.jsx): improve the not found page UI 🐛 fix(page.js): fix indentation and whitespace issues The not found page now has a better UI with a more descriptive message and links to the home page and contact page. In page.js, indentation and whitespace issues were fixed to improve code readability.

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Link from 'next/link'
 
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <div>
+      <h1>
+        The link you clicked may be broken or the page may have been removed
+      </h1>
+      <p>
+        Visit the <Link href="/">home page</Link> or{' '}
+        <Link href="/contact">contact us</Link> if you think this is a mistake.
+      </p>
+    </div>
+  )
 }
 
 export default NotFound
