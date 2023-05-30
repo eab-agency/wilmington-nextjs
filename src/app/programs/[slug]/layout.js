98b047c7f7be65d0d5d03e121d1d4e59bd682ab6
Andrei Olea <aolea@eab.com>
2023-05-30 12:09:03 -0400

    🔥 chore(layout.js): remove unused imports and commented code 🚧 chore(layout.js): comment out unused code and add TODOs for future work

--- src/app/programs/[slug]/layout.js
+++ src/app/programs/[slug]/layout.js
@@ -1,6 +1,6 @@
-import ProgramTabs from '@/components/atoms/ProgramTabs'
+// import ProgramTabs from '@/components/atoms/ProgramTabs'
 import WordPressProvider from '@/components/common/WordPressProvider'
-import PageHero from '@/components/organisms/PageHero/PageHero'
+// import PageHero from '@/components/organisms/PageHero/PageHero'
 import getProgramChildrenByID from '@/functions/wordpress/programs/getProgramChildrenById'
 
 const programLayout = async ({ children, params }) => {
@@ -20,16 +20,35 @@ const programLayout = async ({ children, params }) => {
     studentOrganizations: programOrgRelationship?.programorg
   }
 
+  console.log(departments)
+
   return (
     <div>
       <article className="inner-wrap">
-        <PageHero
+        {/* TODO: AO featured image figure is set to object-fit cover. doesn't display but is on the page  */}
+        {/* {featuredImage && <FeaturedImage image={featuredImage.node} />}
+        {program && (
+          <ul>
+            <li>
+              <Link href={uri}>{title}</Link>
+            </li>
+            {childPages &&
+              childPages.nodes.map((childPage) => {
+                return (
+                  <li key={childPage.title}>
+                    <Link href={childPage.uri}>{childPage?.title}</Link>
+                  </li>
+                )
+              })}
+          </ul>
+        )} */}
+        {/* <PageHero
           text={title}
           sourceUrl={featuredImage.node?.sourceUrl}
           altText={featuredImage.node?.altText}
           imageMeta={featuredImage.node?.mediaDetails}
-        />
-        <ProgramTabs childPages={childPages} uri={uri} />
+        /> */}
+        {/* <ProgramTabs childPages={childPages} uri={uri} /> */}
         <WordPressProvider value={wpInitialState}>{children}</WordPressProvider>
       </article>
     </div>
