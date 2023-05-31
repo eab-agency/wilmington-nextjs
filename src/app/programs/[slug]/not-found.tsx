cd232278e10b86902624d714cb38d94fb6c03a5a
Bob_Smith <reversetype@gmail.com>
2023-05-31 11:08:32 -0400

    🔨 refactor(programs/not-found.tsx): refactor not-found page to show other programs 🔨 refactor(ButtonProgramDropdown.js): comment out unused code The not-found page has been refactored to show other programs that the user might be interested in. The page now displays a list of departments and their programs. The ButtonProgramDropdown component has been refactored to comment out unused code.

--- src/app/programs/[slug]/not-found.tsx
+++ src/app/programs/[slug]/not-found.tsx
@@ -1,3 +1,48 @@
-export default function NotFound() {
-  return <p>Uh oh! This program could not be found</p>
+import getPostTypeArchive from '@/functions/wordpress/postTypes/getPostTypeArchive'
+import Link from 'next/link'
+
+// TODO: AO: We probably want to make these into their own components
+
+const Programs = ({ programs }: { programs: any[] }) => {
+  return (
+    <div>
+      {programs.map((program, index) => (
+        <div key={index}>
+          <h3>
+            <Link href={program.uri}>{program.title}</Link>
+          </h3>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+const DepartmentItem = ({ department }: { department: any }) => {
+  const { programs } = department
+
+  if (programs.nodes.length === 0) {
+    return null
+  }
+  return (
+    <div key={department.databaseId}>
+      <h2>{department.name}</h2>
+      <p>{department.description}</p>
+      <Programs programs={programs.nodes} />
+    </div>
+  )
+}
+
+export default async function NotFound() {
+  const { posts }: { posts: any[] } = await getPostTypeArchive('department')
+
+  return (
+    <>
+      <h1>Uh oh! This program could not be found</h1>
+      <p>Here are some other programs you might be interested in:</p>
+      {posts.map((department, index) => (
+        <DepartmentItem department={department} key={index} />
+      ))}
+      {/* <DirectoryDropDown programDepartments={posts} /> */}
+    </>
+  )
 }
