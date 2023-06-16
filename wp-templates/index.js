4f6cac07156adeb7e3114e833fbe7f30a8aca696
Bob_Smith <reversetype@gmail.com>
2023-06-15 22:09:33 -0400

    🔧 chore(server.ts): update eslint-disable comment 🚧 feat(program-hold): add program child page and layout components 🚧 feat(program-hold): add not-found component 🔧 chore(Image.js): remove unused PropTypes import 🔧 chore(BlockTestimonial.js): remove console.log statement 🚧 feat(getImageSizeProps.js): add full and full-size image sizes 🚧 feat(wp-templates): add SingleProgram template The changes include adding new components for the program-hold section of the application, including program child pages, layout components, and a not-found component. Unused PropTypes import was removed from the Image.js file. A console.log statement was removed from the BlockTestimonial.js file. The getImageSizeProps.js file was updated to include full and full-size image sizes. A new SingleProgram template was added to the wp-templates directory.
    🎨 style(page.js, single-program.js): format code and add imports
    This commit adds imports for new components and formats the code in the page.js and single-program.js files. No functional changes were made.

--- wp-templates/index.js
+++ wp-templates/index.js
@@ -1,11 +1,13 @@
 import frontPage from './front-page'
 import page from './page'
 import singular from './single'
+import SingleProgram from './single-program'
 
 const templates = {
   'front-page': frontPage,
   page,
-  singular
+  singular,
+  'single-program': SingleProgram
 }
 
 export default templates
