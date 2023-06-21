8aa9c666a1a9407cc66ec5cfa7cadd597021b0ee
Andrei Olea <aolea@eab.com>
2023-06-21 13:52:47 -0400

    🎨 style(BlockRelatedPrograms.js): remove unnecessary text from Related Programs header

--- src/components/blocks/acf/BlockRelatedPrograms/BlockRelatedPrograms.js
+++ src/components/blocks/acf/BlockRelatedPrograms/BlockRelatedPrograms.js
@@ -37,7 +37,7 @@ export default function BlockRelatedPrograms({ departments }) {
   return (
     <section className={styles.relatedPrograms}>
       <div className={styles.programsContainer}>
-        <h2>Related Programs XXXXXX</h2>
+        <h2>Related Programs</h2>
         <MultiCarousel
           responsive={responsive}
           infinite
