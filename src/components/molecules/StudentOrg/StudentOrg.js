0482bac20d069af48ffa40f259d5f59a2df006fa
Andrei Olea <aolea@eab.com>
2023-06-23 18:26:47 -0400

    ✨ feat(styles): add styles for student organizations section ✨ feat(wp-blocks): add student organizations section to the page

--- src/components/molecules/StudentOrg/StudentOrg.js
+++ src/components/molecules/StudentOrg/StudentOrg.js
@@ -1,22 +1,30 @@
 'use client'
 
-import React, { useState } from 'react'
 import Button from '@/components/atoms/Buttons/Button'
 import Heading from '@/components/atoms/Heading'
+import React, { useState } from 'react'
+import { MdAdd, MdRemove } from 'react-icons/md'
 
 function StudentOrg({ heading, url, quickFact }) {
   const [isOpen, setIsOpen] = useState(false)
   const handleOpen = () => setIsOpen(!isOpen)
 
+  // add icon to button
+
   const ShowHideButton = () => (
-    <button aria-expanded={isOpen} onClick={handleOpen}>
-      <span>{isOpen ? '- Hide' : '+ Show'} </span>Quick Facts
+    <button
+      type="button"
+      className="quickFactsBtn"
+      aria-expanded={isOpen}
+      onClick={handleOpen}
+    >
+      {isOpen ? 'Hide quick facts' : 'Show quick facts'}{' '}
+      <i>{isOpen ? <MdRemove /> : <MdAdd />} </i>
     </button>
   )
 
   const QuickFact = ({ fact }) => (
-    <>
-      <ShowHideButton />
+    <div className="studentOrgAccordion">
       <div
         id="accordion-content"
         aria-labelledby="accordion-button"
@@ -24,15 +32,17 @@ function StudentOrg({ heading, url, quickFact }) {
         role="region"
         dangerouslySetInnerHTML={{ __html: fact }}
       />
-    </>
+      <ShowHideButton />
+    </div>
   )
   return (
-    <>
-      <pre>FILE: StudentOrg.txs</pre>
-      <Heading tag="h3">{heading}</Heading>
+    <article className="studentOrg">
+      <div className="studentOrgContent">
+        <Heading tag="h3">{heading}</Heading>
+        {quickFact && <QuickFact fact={quickFact} />}
+      </div>
       <Button url={url} text="Visit Org" />
-      {quickFact && <QuickFact fact={quickFact} />}
-    </>
+    </article>
   )
 }
 
