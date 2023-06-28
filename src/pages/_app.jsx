08208cefbf59c4b4ff91562fc82de648dd1a798a
Bob_Smith <reversetype@gmail.com>
2023-06-28 00:37:00 -0400

    🎨 style(_app.jsx): rename MyApp component to WilmingtonApp 🔊 chore(_app.jsx): add console logs for branding and application link The MyApp component has been renamed to WilmingtonApp to improve clarity and reflect the specific application being used. Two console logs have been added to display a branded message and a link to apply for admission. These logs serve as a visual representation of the branding and provide a direct link for users to apply.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,7 +8,14 @@ import React from 'react'
 import '../../faust.config'
 import blocks from '../../wp-blocks'
 
-export default function MyApp({ Component, pageProps }) {
+export default function WilmingtonApp({ Component, pageProps }) {
+  console.log(
+    '%cWilmginton College',
+    'color: rgb(142, 198, 64);font-size: 30px;font-weight: bold;text-shadow: 1px 1px 5px rgb(0, 0, 0);filter: dropshadow(color=rgb(0, 198, 0), offx=1, offy=1);'
+  )
+  console.log(
+    'You have what it takes. Apply now!\nhttps://www.wilmington.edu/admission/apply/'
+  )
   const router = useRouter()
 
   return (
