58f5a34f56443d4f8d8675ef2ed5e6372143f31c
Bob_Smith <reversetype@gmail.com>
2023-06-28 21:19:24 -0400

    🚀 feat(QuickFact.js): add QuickFact component 🚀 feat(index.js): export QuickFact component 🚀 feat(BlockDepartmentSelect.js): import and use RelatedProgramCard component 🚀 feat(RelatedProgramCard.tsx): add QuickFact component 🚀 feat(StudentOrg.js): remove unused imports and add QuickFact component 🚀 feat(EabProgramDirectory.js): add excerpt field to program query
    The QuickFact component is added to the project, allowing the display of additional information in an accordion-style format. The BlockDepartmentSelect component now uses the RelatedProgramCard component to display related programs. The StudentOrg component removes unused imports and adds the QuickFact component. The EabProgramDirectory component now includes the excerpt field in the program query, providing a summary of each program.

--- src/components/molecules/StudentOrg/StudentOrg.js
+++ src/components/molecules/StudentOrg/StudentOrg.js
@@ -1,40 +1,8 @@
-'use client'
-
 import Button from '@/components/atoms/Buttons/Button'
 import Heading from '@/components/atoms/Heading'
-import React, { useState } from 'react'
-import { MdAdd, MdRemove } from 'react-icons/md'
+import QuickFact from '@/components/atoms/QuickFact'
 
 function StudentOrg({ heading, url, quickFact }) {
-  const [isOpen, setIsOpen] = useState(false)
-  const handleOpen = () => setIsOpen(!isOpen)
-
-  // add icon to button
-
-  const ShowHideButton = () => (
-    <button
-      type="button"
-      className="quickFactsBtn"
-      aria-expanded={isOpen}
-      onClick={handleOpen}
-    >
-      {isOpen ? 'Hide quick facts' : 'Show quick facts'}{' '}
-      <i>{isOpen ? <MdRemove /> : <MdAdd />} </i>
-    </button>
-  )
-
-  const QuickFact = ({ fact }) => (
-    <div className="studentOrgAccordion">
-      <div
-        id="accordion-content"
-        aria-labelledby="accordion-button"
-        hidden={!isOpen}
-        role="region"
-        dangerouslySetInnerHTML={{ __html: fact }}
-      />
-      <ShowHideButton />
-    </div>
-  )
   return (
     <article className="studentOrg">
       <div className="studentOrgContent">
