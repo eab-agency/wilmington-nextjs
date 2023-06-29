7d5a4a5223bcf0c470a0a2037689558a0cb99999
Bob_Smith <reversetype@gmail.com>
2023-06-29 17:48:55 -0400

    🔧 chore(BlockFeaturedPrograms.js, BlockRelatedPrograms.js, index.js): update program directory URL 🔥 Remove commented out 'archive-program' template from index.js The program directory URL in the BlockFeaturedPrograms and BlockRelatedPrograms components has been updated to "/academics/program-directory" to reflect the correct URL path. Additionally, the commented out 'archive-program' template has been removed from the index.js file.

--- src/components/blocks/acf/BlockRelatedPrograms/BlockRelatedPrograms.js
+++ src/components/blocks/acf/BlockRelatedPrograms/BlockRelatedPrograms.js
@@ -54,7 +54,7 @@ export default function BlockRelatedPrograms({ departments }) {
         </MultiCarousel>
         <div className={styles.viewAllCta}>
           <Button
-            url="/programs"
+            url="/academics/program-directory"
             className="secondary"
             text="View All Programs"
           />
