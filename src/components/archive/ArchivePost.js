be10b804d336d04f641a62d4640e3ac35799c31b
Andrei Olea <aolea@eab.com>
2023-06-30 06:36:40 -0400

    🔧 chore(ArchivePost.js): remove unused imports and commented out code ✨ feat(ArchivePost.js): add support for displaying featured image and post title in archive post ✨ feat(ArchivePost.js): add support for displaying post content and meta in archive post ✨ feat(ArchivePost.js): add support for displaying "Read More" button in archive post 🔧 chore(ResultProgramCard.module.scss): delete unused file ✨ feat(_archive.scss): add styles for archive layout ✨ feat(_eventsListing.scss): add font-weight: bold to articleLink class ✨ feat(styles.scss): import archive layout styles 🔧 chore(styles.scss): remove unused imports ✨ feat(archive.js): add support for displaying archive title and description ✨ feat(archive.js): add support for displaying list of posts in archive layout

--- src/components/archive/ArchivePost.js
+++ src/components/archive/ArchivePost.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-unused-vars */
+import Image from '@/components/atoms/Image/Image'
 import React from 'react'
-import FeaturedImage from '../common/FeaturedImage'
+// import FeaturedImage from '../common/FeaturedImage'
+// import { className } from 'classnames/bind'
+import Link from 'next/link'
 import { PostEntryContent } from '../post/PostEntryContent'
 import { PostEntryMeta } from '../post/PostEntryMeta'
-import { PostEntryTitle } from '../post/PostEntryTitle'
+// import { PostEntryTitle } from '../post/PostEntryTitle'
+import Button from '@/components/atoms/Buttons/Button'
+import { className } from 'classnames/bind'
 
 const ArchivePost = ({
   isFirst = false,
@@ -12,13 +17,31 @@ const ArchivePost = ({
   ctx,
   ...props
 }) => {
+  const { altText, sourceUrl, mediaDetails } = post.featuredImage?.node || {}
+
+  const classes = className ? `${className} archiveArticle` : 'archiveArticle'
+
   return (
-    <article className={className} {...props}>
-      {/* could use thumbnail image instead. might reserve FeaturedImage to only be for full post */}
-      {post.image && <FeaturedImage post={post} />}
-      <PostEntryTitle post={post} location="archive" />
-      <PostEntryContent post={post} location="archive" />
-      <PostEntryMeta post={post} />
+    <article className={classes} {...props}>
+      <div className="archiveCardHead">
+        {post.featuredImage && (
+          <Image url={sourceUrl} alt={altText} imageMeta={{ mediaDetails }} />
+        )}
+        <h2 className="archiveCardTitle">{post.title}</h2>
+      </div>
+      <div className="archiveCardContent">
+        <PostEntryContent post={post} location="archive" />
+        <PostEntryMeta post={post} />
+
+        <div className="groupBtn">
+          <Button
+            className="articleLink"
+            url={post.uri}
+            type="regularlink"
+            text="Read More"
+          />
+        </div>
+      </div>
     </article>
   )
 }
