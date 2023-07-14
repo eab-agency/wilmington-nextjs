55f233311fc916d54bbdca31b0e8899d3271d1ed
Bob_Smith <reversetype@gmail.com>
2023-07-14 12:47:09 -0400

    🔀 refactor(archive-program.js): remove unused imports and update component logic 🔀 chore(index.js): uncomment 'archive-program' template The 'archive-program.js' file has been refactored to remove unused imports and update the component logic. The unused imports have been removed to improve code cleanliness. The component logic has been simplified to redirect to the '/academics/program-directory' route using the useRouter hook from the 'next/navigation' package. The 'index.js' file has been updated to uncomment the 'archive-program' template, allowing it to be used in the application.

--- wp-templates/archive-program.js
+++ wp-templates/archive-program.js
@@ -1,58 +1,11 @@
-import { SEO } from '@/components'
-import RichText from '@/components/atoms/RichText'
-import BlockDepartmentSelect from '@/components/blocks/custom/BlockDepartmentSelect'
-import Layout from '@/components/common/Layout'
-import { BlogInfoFragment } from '@/fragments/GeneralSettings'
-import { gql, useQuery } from '@apollo/client'
-import EabProgramDirectory from '../wp-blocks/custom/EabProgramDirectory'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
-export default function ArchivePrograms(props) {
-  const { loading, error, data } = useQuery(EabProgramDirectory.query)
-
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error: {error.message}</p>
-
-  const { label, contentNodes } = props.data.nodeByUri
-
-  const { title: siteTitle, description: siteDescription } =
-    props?.data?.generalSettings ?? {}
-  return (
-    <>
-      <SEO seo={{ title: siteTitle, description: siteDescription }} />
-      <Layout className="thelayoutclass">
-        <article className="inner-wrap">
-          <RichText tag="h1">{label}</RichText>
-          <BlockDepartmentSelect programDepartments={data?.departments.nodes} />
-        </article>
-      </Layout>
-    </>
-  )
+export default function ArchivePrograms() {
+  const { push } = useRouter()
+  useEffect(() => {
+    push('/academics/program-directory')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  return <p></p>
 }
-
-ArchivePrograms.variables = ({ uri }) => {
-  return { uri }
-}
-
-ArchivePrograms.query = gql`
-  ${BlogInfoFragment}
-  query ProgramArchive($uri: String!) {
-    nodeByUri(uri: $uri) {
-      ... on ContentType {
-        label
-        description
-        contentNodes {
-          nodes {
-            databaseId
-            uri
-            ... on NodeWithTitle {
-              title
-            }
-          }
-        }
-      }
-    }
-    generalSettings {
-      ...BlogInfoFragment
-    }
-  }
-`
