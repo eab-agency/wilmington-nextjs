99f21417fa682dae8875ca32cfd6ca3479752592
Bob_Smith <reversetype@gmail.com>
2023-07-24 16:10:40 -0400

    log redirects

--- next.config.js
+++ next.config.js
@@ -12,6 +12,11 @@ const nextConfig = {
   },
   async redirects() {
     const redirects = await fetchRedirects()
+    // eslint-disable-next-line no-console
+    console.log(
+      '🚀 ~ file: next.config.js:15 ~ redirects ~ redirects:',
+      redirects
+    )
     return [...redirects]
   },
   reactStrictMode: true,
