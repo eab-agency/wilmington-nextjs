c96ae2ccea013d984310a33d8912d3d29856f3f2
Bob_Smith <reversetype@gmail.com>
2023-07-27 14:14:21 -0400

    remove log

--- src/components/archive/EventsPostCard.js
+++ src/components/archive/EventsPostCard.js
@@ -18,8 +18,7 @@ const EventsPost = ({
   if (!post) return null
 
   const { title, featuredImage, uri, eventsFields, multiDay } = post
-  // eslint-disable-next-line no-console
-  if (multiDay) console.log('multiDay: ', multiDay)
+
   // TODO: Andrei, i've added a 'multiDay' field to the post object if you want to display dates differently for multi-day events
 
   const { node: { sourceUrl, altText, mediaDetails } = {} } =
