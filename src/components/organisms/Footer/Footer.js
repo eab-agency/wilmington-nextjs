bfc87ed57d62c1c12b961dea8189e625283e92f8
Andrei Olea <aolea@eab.com>
2023-07-31 13:12:05 -0400

    🔧 chore(AlgoliaSearch.module.scss): comment out unused styles 🔧 chore(Footer.js): update YouTube link in Footer component 🔧 chore(_icons.scss): update trash icon variable name 🔧 chore(archive-faculty.js): update archive title in ArchiveFaculty component

--- src/components/organisms/Footer/Footer.js
+++ src/components/organisms/Footer/Footer.js
@@ -69,7 +69,7 @@ const Footer = ({ menus }) => {
               </li>
               <li>
                 <a
-                  href="https://www.youtube.com/user/WilmingtonCollege"
+                  href="https://www.youtube.com/@wilmingtoncollege1870"
                   target="_blank"
                   rel="noopener noreferrer"
                   className={styles.socialLink}
