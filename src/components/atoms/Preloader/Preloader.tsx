ffb29a932f8a62b47884a74fc3d71707b2522f7d
Andrei Olea <aolea@eab.com>
2023-08-16 23:22:21 -0400

    🔥 refactor(Preloader.tsx): remove commented out code for old preloader design

--- src/components/atoms/Preloader/Preloader.tsx
+++ src/components/atoms/Preloader/Preloader.tsx
@@ -4,16 +4,6 @@ import styles from './Preloader.module.scss'
 export default function Preloader() {
   return (
     <div className={styles.preloaderContainer}>
-      {/* <div className={styles.preloader}>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div> */}
       <div className={styles.preloader2}>
         <div></div>
         <div></div>
