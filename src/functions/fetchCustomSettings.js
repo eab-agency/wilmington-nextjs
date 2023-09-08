1ca9e87497aeaaae7b49ba66925739bce5102287
Bob_Smith <reversetype@gmail.com>
2023-09-08 09:03:12 -0400

    🐛 fix(fetchCustomSettings.js): add 'cache-control' header to fetch request The 'cache-control' header is added to the fetch request to specify that the response should not be cached. This ensures that the latest custom settings JSON is always fetched from the server.

--- src/functions/fetchCustomSettings.js
+++ src/functions/fetchCustomSettings.js
@@ -5,7 +5,8 @@ async function fetchCustomSettingsJSON() {
   const response = await fetch(GRAPHQL_ENDPOINT, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'cache-control': 'no-store' // Add this line
     },
     body: JSON.stringify({
       query: customSettingsFields
