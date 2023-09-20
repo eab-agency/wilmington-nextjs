9646575b4e1c315ab778ba8a83740557224d42c2
Bob_Smith <reversetype@gmail.com>
2023-09-20 10:02:07 -0400

    🔥 refactor(NewsPost.js): remove unused import of TheDate component 🔥 refactor(NewsPostCard.js): remove unused import of TheDate component The import statement for the unused TheDate component has been removed from both NewsPost.js and NewsPostCard.js. This improves code cleanliness and removes unnecessary dependencies.

--- src/components/archive/NewsPostCard.js
+++ src/components/archive/NewsPostCard.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 import Button from '@/components/atoms/Buttons/Button'
 import Image from '@/components/atoms/Image'
-import TheDate from '@/components/atoms/TheDate'
 import Link from 'next/link'
 
 const NewsPost = ({
