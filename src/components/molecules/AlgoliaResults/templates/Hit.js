44e261fa57b3ffb5d58c3bcc7ac04093d6c13208
Bob_Smith <reversetype@gmail.com>
2023-10-10 14:50:24 -0400

    formatting

--- src/components/molecules/AlgoliaResults/templates/Hit.js
+++ src/components/molecules/AlgoliaResults/templates/Hit.js
@@ -1,6 +1,6 @@
-import Link from '@/components/common/Link';
-import { Highlight } from 'react-instantsearch-dom';
-import * as styles from '../AlgoliaResults.module.scss';
+import Link from '@/components/common/Link'
+import { Highlight } from 'react-instantsearch-dom'
+import * as styles from '../AlgoliaResults.module.scss'
 
 /**
  * Render the Hit component.
@@ -12,13 +12,17 @@ import * as styles from '../AlgoliaResults.module.scss';
 export default function Hit({ hit }) {
   return (
     <div className={styles.hit}>
-
-      <Link href={hit.permalink}><h3
-        dangerouslySetInnerHTML={{
-          __html: hit._highlightResult.post_title.value
-        }}
-      /></Link>
-      <div className={styles.date} dangerouslySetInnerHTML={{ __html: hit._snippetResult.content.value }} />
-    </div >
+      <Link href={hit.permalink}>
+        <h3
+          dangerouslySetInnerHTML={{
+            __html: hit._highlightResult.post_title.value
+          }}
+        />
+      </Link>
+      <div
+        className={styles.date}
+        dangerouslySetInnerHTML={{ __html: hit._snippetResult.content.value }}
+      />
+    </div>
   )
 }
