c89e391c351a7eeb0882baf773677761bfffda37
Andrei Olea <aolea@eab.com>
2023-10-30 09:53:59 -0400

    🐛 fix(EabProgramJumplinks.js): add event listener for jump links to scroll smoothly to target ✨ feat(EabProgramJumplinks.js): add cleanup function to remove event listener for jump links

--- wp-blocks/custom/EabProgramJumplinks.js
+++ wp-blocks/custom/EabProgramJumplinks.js
@@ -1,16 +1,45 @@
 import { gql } from '@apollo/client';
+import { useEffect } from 'react';
 
 const EabProgramJumplinks = (props) => {
   const { jumpLinks } = props.attributes
   const parsedJumpLinks = JSON.parse(jumpLinks);
+
+  useEffect(() => {
+    const headHeight = document.querySelector('#page-header').offsetHeight;
+    const jumpLinksGroup = document.querySelectorAll('.jumpLink');
+
+    const handleClick = (e) => {
+      e.preventDefault();
+      const target = document.querySelector(e.target.getAttribute('href'));
+      const targetOffset = target.getBoundingClientRect().top + window.pageYOffset - headHeight;
+      window.scrollTo({
+        top: targetOffset,
+        behavior: "smooth"
+      });
+    }
+
+    jumpLinksGroup.forEach(link => {
+      link.addEventListener('click', handleClick);
+    });
+
+    return () => {
+      jumpLinksGroup.forEach(link => {
+        link.removeEventListener('click', handleClick);
+      });
+    }
+  }, []);
+
+
+
   return (
     <div className="wp-block-eab-program-jumplinks">
       <section>
-        <h3 className="wp-block-eab-program-jumplinks">On this page</h3>
+        <h3>On this page</h3>
         <ul>
           {parsedJumpLinks.map((block, index) => (
             <li key={index}>
-              <a href={`#${block.id}`}>{block.rawHtml}</a>
+              <a href={`#${block.id}`} className='jumpLink'>{block.rawHtml}</a>
             </li>
           ))}
         </ul>
