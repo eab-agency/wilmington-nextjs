e37f262ad6c2902cc3f315837f3130624ac3e9bd
Andrei Olea <andrei@andreiolea.com>
2023-12-19 00:53:53 -0500

    🐛 fix(ChildrenPrograms.tsx): add missing comma after parentSlug parameter 🐛 fix(ChildrenPrograms.tsx): add missing comma after parentDegreeType parameter ✨ feat(ChildrenPrograms.tsx): filter programs with ancestor and same degree type ✨ feat(ChildrenPrograms.tsx): add support for concentrationEnabled check ✨ feat(ProgramDirectory.tsx): add support for concentrationEnabled check ✨ feat(ProgramDirectory.tsx): add support for filtering programs with parent and concentrationEnabled is false 🔧 chore(_blockDepartmentSelect.scss): fix quotes and indentation 🔧 chore(EabProgramDirectory.js): remove unnecessary code

--- src/components/program/ProgramDirectory.tsx
+++ src/components/program/ProgramDirectory.tsx
@@ -1,4 +1,5 @@
 import ModalityIcons from '@/components/atoms/ModalityIcons/ModalityIcons'
+import { randomInt } from 'crypto'
 import Link from 'next/link'
 import React from 'react'
 import ChildrenPrograms from './ChildrenPrograms'
@@ -20,6 +21,8 @@ export default function ProgramDirectory({ programs }: ProgramDirectoryProps) {
       degreeTypeName = program.degreeTypes.edges[0].node.name
     }
 
+    console.log('degreeTypeName', degreeTypeName)
+
     return degreeTypeName
   }
 
@@ -27,55 +30,67 @@ export default function ProgramDirectory({ programs }: ProgramDirectoryProps) {
     const degreeType = getProgramDegreeType(program)
 
     //return all programs that have the same degree type and don't have a parent
+    // also return all programs that have a parent but concentrationEnabled is false
 
-    if (!program.ancestors?.nodes.length) {
+    if (
+      !program.ancestors?.nodes.length ||
+      (program.ancestors?.nodes.length &&
+        program.concentrationEnabled === false)
+    ) {
       if (!acc[degreeType]) {
         acc[degreeType] = []
       }
       acc[degreeType].push(program)
     }
 
+
+
     return acc
   }, {})
+
+  //
 
   return (
     <div className="programDirectory">
       <h2>ProgramDirectory</h2>
       {Object.keys(groupedPrograms).map((degreeType) => (
-        <section key={degreeType} className="programTableContainer">
-          <h3 className="degreeTypeName">{degreeType}s</h3>
-          <table className="programTable">
-            <thead>
-              <tr>
-                <th>Degrees</th>
-                <th>Modality</th>
-              </tr>
-            </thead>
-            <tbody>
-              {groupedPrograms[degreeType].map((program: any) => (
-                <>
-                  <tr key={program.slug} className="parentProgram">
-                    <td>
-                      <Link href={program.uri} className="tableProgramTitle">
-                        <h4>{program.title} </h4>
-                      </Link>
-                    </td>
-                    <td>
-                      <span aria-hidden="true" className="tableCellHead">
-                        Modality:
-                      </span>
-                      <ModalityIcons modalities={program.modalities} />
-                    </td>
-                  </tr>
-                  <ChildrenPrograms
-                    programs={programs}
-                    parentSlug={program.slug}
-                  />
-                </>
-              ))}
-            </tbody>
-          </table>
-        </section>
+        <React.Fragment key={degreeType}>
+          <section className="programTableContainer">
+            <h3 className="degreeTypeName">{degreeType}</h3>
+            <table className="programTable">
+              <thead>
+                <tr>
+                  <th>Degrees</th>
+                  <th>Modality</th>
+                </tr>
+              </thead>
+              <tbody>
+                {groupedPrograms[degreeType].map((program: any) => (
+                  <React.Fragment key={program.slug}>
+                    <tr className="parentProgram">
+                      <td>
+                        <Link href={program.uri} className="tableProgramTitle">
+                          <h4>{program.title}</h4>
+                        </Link>
+                      </td>
+                      <td>
+                        <span aria-hidden="true" className="tableCellHead">
+                          Modality:
+                        </span>
+                        <ModalityIcons modalities={program.modalities} />
+                      </td>
+                    </tr>
+                    <ChildrenPrograms
+                      programs={programs}
+                      parentSlug={program.slug}
+                      parentDegreeType={degreeType}
+                    />
+                  </React.Fragment>
+                ))}
+              </tbody>
+            </table>
+          </section>
+        </React.Fragment>
       ))}
 
       {programs.filter((program: any) => !program.degreeTypes.edges.length)
