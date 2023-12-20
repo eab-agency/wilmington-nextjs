0a4e44d812c970a46a45f49dd6d677ae49a74e47
Andrei Olea <andrei@andreiolea.com>
2023-12-20 11:47:21 -0500

    🐛 fix(ModalityIcons.tsx): fix import paths for icons and remove unnecessary semicolons 🐛 fix(CollegeHallIcon.tsx): fix import path for SvgIcon 🐛 fix(SvgIcon.tsx): fix import path for React and remove unnecessary semicolons 🐛 fix(index.ts): fix import path for SvgIcon 🐛 fix(_blockDepartmentSelect.scss): fix import paths for sass and tokens, and replace double quotes with single quotes

--- src/components/atoms/SvgIcon/SvgIcon.tsx
+++ src/components/atoms/SvgIcon/SvgIcon.tsx
@@ -1,23 +1,30 @@
-import { SVGProps } from "react";
+import React, { SVGProps } from 'react'
 
-export const SvgIcon = ({ children, width, height, size, viewBox, ...props }: SVGIconProps) => {
+export const SvgIcon = ({
+  children,
+  width,
+  height,
+  size,
+  viewBox,
+  ...props
+}: SVGIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox={viewBox || "0 0 70 70"}
+      viewBox={viewBox || '0 0 70 70'}
       // width={width || size || "70"}
       // height={height || size || "70"}
       {...props}
     >
       {children}
     </svg>
-  );
+  )
 }
 
 export interface SVGIconProps extends SVGProps<SVGSVGElement> {
-  size?: string | number;
-  children?: React.ReactNode;
-  viewBox?: string;
-  width?: string | number;
-  height?: string | number;
+  size?: string | number
+  children?: React.ReactNode
+  viewBox?: string
+  width?: string | number
+  height?: string | number
 }
