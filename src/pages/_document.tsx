330250944064fe4cc0343e9c3e7f3ecd0163ea33
Bob Smith <reversetype@gmail.com>
2025-03-18 13:37:04 -0400

    🔧 chore: update .gitignore, enhance formstack styles, integrate New Relic monitoring, and configure application logging

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,59 @@
-import { Head, Html, Main, NextScript } from 'next/document'
+import newrelic from 'newrelic'
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document'
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <link
-          href="https://fonts.googleapis.com/css2?&family=Roboto+Slab&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const initialProps = await Document.getInitialProps(context)
+
+    if (newrelic.agent.collector.isConnected() === false) {
+      await new Promise((resolve) => {
+        newrelic.agent.on('connected', resolve)
+      })
+    }
+
+    const browserTimingHeader = newrelic.getBrowserTimingHeader({
+      hasToRemoveScriptWrapper: true,
+      allowTransactionlessInjection: true
+    })
+
+    return {
+      ...initialProps,
+      browserTimingHeader
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?&family=Roboto+Slab&display=swap"
+            rel="stylesheet"
+          />
+
+          <script
+            type="text/javascript"
+            // The body of the script element comes from the async evaluation
+            // of `getInitialProps`. We use the special
+            // `dangerouslySetInnerHTML` to provide that element body. Since
+            // it requires an object with an `__html` property, we pass in an
+            // object literal.
+            dangerouslySetInnerHTML={{
+              __html: (this.props as any).browserTimingHeader
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
