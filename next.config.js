bd3e3376477f1490e69f79ba19eda7c34c9f6b04
Bob Smith <reversetype@gmail.com>
2025-03-20 00:33:44 -0400

    🔧 feat(New Relic): externalize New Relic modules in webpack and update build scripts for integration

--- next.config.js
+++ next.config.js
@@ -5,8 +5,16 @@ const { withFaust } = require('@faustwp/core')
 const remotePatterns = require('./src/config/imageConfig')
 const path = require('path')
 const glob = require('glob')
+const nrExternals = require('newrelic/load-externals')
 
 const nextConfig = {
+  // In order for newrelic to effectively instrument a Next.js application,
+  // the modules that newrelic supports should not be mangled by webpack. Thus,
+  // we need to "externalize" all of the modules that newrelic supports.
+  webpack: (config) => {
+    nrExternals(config)
+    return config
+  },
   experimental: {
     instrumentationHook: true
   },
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,17 +12,12 @@ type NewRelicProps = {
   browserTimingHeader: string
 }
 
-class MyDocument extends Document<NewRelicProps> {
+class RootDocument extends Document<NewRelicProps> {
   static async getInitialProps(
     ctx: DocumentContext
   ): Promise<DocumentInitialProps & NewRelicProps> {
     const initialProps = await Document.getInitialProps(ctx)
 
-    /**
-     * For SSG pages the build is faster than the agent connect cycle
-     * In those cases, let's wait for the agent to connect before getting
-     * the browser agent script.
-     */
     // @ts-ignore
     if (!newrelic.agent.collector.isConnected()) {
       await new Promise((resolve) => {
@@ -64,4 +59,4 @@ class MyDocument extends Document<NewRelicProps> {
   }
 }
 
-export default MyDocument
+export default RootDocument
