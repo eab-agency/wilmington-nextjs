cfa048eee6600941d99821e58184c73a8ce8ebd2
Andrei Olea <andrei@andreiolea.com>
2025-09-12 16:42:40 -0400

    Remove console logs from EabBlocksAccordion for cleaner code

--- wp-blocks/custom/EabBlocksAccordion.js
+++ wp-blocks/custom/EabBlocksAccordion.js
@@ -3,14 +3,11 @@ import Accordion from '@/components/molecules/Accordian/Accordian'
 import { gql } from '@apollo/client'
 
 const EabBlocksAccordion = (props) => {
-  // For now, let's log the props to see the actual structure
-  console.log('EabBlocksAccordion props:', props.attributes)
+
 
   // Extract title and content from the props structure
   const attributes = props.attributes || {}
   const { accordionTitle = 'Accordion Title', accordionContent = 'Accordion Content' } = attributes
-
-  console.log('Accordion title:', accordionTitle)
 
   return (
     <Accordion title={accordionTitle}>
