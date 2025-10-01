6301be4c995dc64531172b1d8c05d995d4ff1f2f
Bob Smith <reversetype@gmail.com>
2025-10-01 11:16:29 -0400

    ✨ (settings.local.json): ensure proper formatting by adding newline at end of file 💡 (document.tsx): add robots meta tag for non-production environments

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const isProduction = process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'
+
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        {!isProduction && <meta name="robots" content="noindex, nofollow" />}
+      </Head>
       <body>
         <Main />
         <NextScript />
